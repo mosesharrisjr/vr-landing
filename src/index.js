81af144eff4210427a6e9eaeed83e75b9fc19da3
Moses Harris <Moses.Harris@ibm.com>
2018-01-29 20:50:54 -0600

    ????

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'babel-polyfill';
 import App from './containers/App';
 import { createBrowserHistory, createHashHistory } from 'history';
 import { Provider } from 'react-redux';
